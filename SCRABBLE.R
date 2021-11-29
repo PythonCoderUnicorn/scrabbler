7119bf90411b50118962a1727c7a6015084e7347
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2021-11-29 06:34:11 -0700

    Create SCRABBLE.R

---
+++ SCRABBLE.R
@@ -0,0 +1,34 @@
+# --------------- scrabbler
+# work in progress
+
+
+L = LETTERS
+
+LetterA = 1
+LetterB = 3
+LetterC = 3
+LetterD = 2
+LetterE = 1
+LetterF = 4
+LetterG = 2
+LetterH = 4
+LetterI = 1
+LetterJ = 8
+LetterK = 5
+LetterL = 1
+LetterM = 3
+LetterN = 1
+Letter0 = 1
+LetterP = 3
+LetterQ = 10
+LetterR = 1
+LetterS = 1
+LetterT = 1
+LetterU = 1
+LetterV = 4
+LetterW = 4
+LetterX = 8
+LetterY = 4
+LetterZ = 10
+
+
